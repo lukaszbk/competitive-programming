acc537b48d2486e24b36cb131d3b0a2efb25246a
Łukasz Bieniasz-Krzywiec <lukaszbk@gmail.com>
2021-07-02 22:12:50 -0700

    Setup Bazel workspace.

---
+++ library/numeric/gcd.h
@@ -0,0 +1,29 @@
+#ifndef LIBRARY_NUMERIC_GCD_H_
+#define LIBRARY_NUMERIC_GCD_H_
+
+#include <algorithm>
+
+namespace cpl {
+namespace numeric {
+
+// Computes the greatest comon divisor of two integers.
+// 
+// Assumptions:
+//  - If a >= 0 && b >= 0, then the result is positive.
+//  - If a < 0 || b < 0, then the result can be negative.
+//
+// Complexity:
+//  - Time:  O(log(min(a,b)))
+//  - Space: O(1)
+template <typename T> T gcd(T a, T b) {
+  while (b) {
+    a %= b;
+    std::swap(a, b);
+  }
+  return a;
+}
+
+} // namespace numeric
+} // namespace cpl
+
+#endif // LIBRARY_NUMERIC_GCD_H_
