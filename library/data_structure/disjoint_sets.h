7654705361e107674f643857be90d457caf64034
Łukasz Bieniasz-Krzywiec <lukaszbk@gmail.com>
2021-07-13 22:25:55 -0700

    Implement DisjointSets data structure.

---
+++ library/data_structure/disjoint_sets.h
@@ -0,0 +1,150 @@
+#ifndef LIBRARY_DATA_STRUCTURE_DISJOINT_SETS_H_
+#define LIBRARY_DATA_STRUCTURE_DISJOINT_SETS_H_
+
+#include <algorithm>
+#include <vector>
+
+namespace cpl {
+
+// A DisjointSets represents a partition of {0, 1, 2, ..., n} set.
+//
+// Partition of a set is grouping of its elements into, non-empty,
+// non-overlapping subsets, in such a way that every element is included in
+// exactly one subset.
+//
+// This data structure uses a forest of trees to represent partition of a set.
+// Each tree in the forest corresponds to a separate subset. The root of each
+// tree is the subset representative returned by the Find() operation.
+// The subset representative can change over time, namely when the MergeSets()
+// operation is called.
+//
+// All operations supported by DisjointSets are fast thanks to the fact that the
+// following optimizations are used:
+// 1) Path Compression
+//    When we call Find(a) for element a, we actally find the representative r
+//    for all elements on the path between a and r. The trick is to make the
+//    paths for all those elements shorter, by setting their parents to r.
+// 2) Merge by Size
+//    When we call MergeSets(a, b), we always attach the representative of the
+//    smaller subset to the representative of the larger subset. This guarantees
+//    that height of all trees in the forest is at most O(log n).
+//
+// If both optimizations are enabled, then the amortized cost of each operation
+// is O(alpha(n)). alpha(n) is the inverse Ackermann function, which grows very
+// slowly (it doesn't exceed 4 for all n < 10^600).
+// If only one optimization is enabled, then the amortized cost of each
+// operation is at most O(log n).
+//
+// This class uses CRTP (Curiously Recuring Template Pattern) to implement
+// static polymorphism.
+template <class Implementation>
+class DisjointSetsCrtp {
+ public:
+  // Creates a new singleton subset in the partition.
+  //
+  // Return Value:
+  //  Returns the element that belongs to the created singleton.
+  //
+  // Complexity:
+  //  - Time:  Refer to the class description for details
+  //  - Space: O(1)
+  int MakeSingleton() {
+    hooks()->OnMakeSingleton();
+    parent_or_size_.push_back(-1);
+    return union_size() - 1;
+  }
+
+  // Merges the two specified subsets: the subset in which element a is located,
+  // and the subset in which the element b is located.
+  //
+  // Return Value:
+  //  Returns the representative of the merged subset.
+  //
+  // Complexity:
+  //  - Time:  Refer to the class description for details
+  //  - Space: O(1)
+  int MergeSets(int a, int b) {
+    if (kEnableMergeBySize_ && size(a) > size(b)) {
+      std::swap(a, b);
+    }
+    hooks()->OnMergeSets(a, b);
+    a = Find(a);
+    b = Find(b);
+    parent_or_size_[b] += parent_or_size_[a];
+    parent_or_size_[a] = b;
+    return b;
+  }
+
+  // Finds and returns the representative of the subset that contains element a.
+  //
+  // Complexity:
+  //  - Time:  Refer to the class description for details
+  //  - Space: O(1)
+  int Find(int element) {
+    int root = element;
+    while (parent_or_size_[root] >= 0) {
+      root = parent_or_size_[root];
+    }
+    if (root == element || root == parent_or_size_[element] ||
+        !kEnablePathCompression_) {
+      return root;
+    }
+    hooks()->OnCompressPath(element, root);
+    while (element != root) {
+      const int parent = parent_or_size_[element];
+      parent_or_size_[element] = root;
+      element = parent;
+    }
+    return root;
+  }
+
+  // Returns the parent of the specified element in the forest.
+  int parent(int element) const {
+    if (parent_or_size_[element] >= 0) {
+      return parent_or_size_[element];
+    } else {
+      return element;
+    }
+  }
+
+  // Returns the size of the subset which the specified element is located.
+  int size(int element) { return std::abs(parent_or_size_[Find(element)]); }
+
+  // Returns the total number of elements in the union of the partition.
+  int union_size() const { return parent_or_size_.size(); }
+
+ protected:
+  DisjointSetsCrtp(int n = 0, bool enable_merge_by_size = true,
+                   bool enable_path_compression = true)
+      : kEnableMergeBySize_(enable_merge_by_size),
+        kEnablePathCompression_(enable_path_compression),
+        parent_or_size_(n, -1) {}
+
+  // This hooks is called right before a new singleton subset is created.
+  void OnMakeSingleton() {}
+
+  // This hook is called right before two specified subsets are merged.
+  void OnMergeSets(int element, int parent) {}
+
+  // This hook is called right before a path from element to root is compressed.
+  void OnCompressPath(int element, int root) {}
+
+  const bool kEnableMergeBySize_;
+  const bool kEnablePathCompression_;
+
+  std::vector<int> parent_or_size_;
+
+ private:
+  Implementation* hooks() { return static_cast<Implementation*>(this); }
+};
+
+class DisjointSets : public DisjointSetsCrtp<DisjointSets> {
+ public:
+  DisjointSets(int n = 0, bool enable_merge_by_size = true,
+               bool enable_path_compression = true)
+      : DisjointSetsCrtp(n, enable_merge_by_size, enable_path_compression) {}
+};
+
+}  // namespace cpl
+
+#endif  // LIBRARY_DATA_STRUCTURE_DISJOINT_SETS_H_
---
+++ library/data_structure/disjoint_sets_test.cc
@@ -0,0 +1,186 @@
+#include "library/data_structure/disjoint_sets.h"
+
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <vector>
+
+using ::testing::ElementsAre;
+using ::testing::InSequence;
+
+namespace cpl {
+
+template <class DisjointSets>
+std::vector<int> sizes(DisjointSets& partition) {
+  std::vector<int> result;
+  for (int i = 0; i < partition.union_size(); ++i) {
+    result.push_back(partition.size(i));
+  }
+  return result;
+}
+
+template <class DisjointSets>
+std::vector<int> parents(const DisjointSets& partition) {
+  std::vector<int> result;
+  for (int i = 0; i < partition.union_size(); ++i) {
+    result.push_back(partition.parent(i));
+  }
+  return result;
+}
+
+template <class DisjointSets>
+std::vector<int> representatives(DisjointSets& partition) {
+  std::vector<int> result;
+  for (int i = 0; i < partition.union_size(); ++i) {
+    result.push_back(partition.Find(i));
+  }
+  return result;
+}
+
+TEST(DisjointSets, EmptyInitialization) {
+  DisjointSets partition;
+  ASSERT_EQ(0, partition.union_size());
+}
+
+TEST(DisjointSets, IterativeInitialization) {
+  DisjointSets partition;
+  ASSERT_EQ(0, partition.union_size());
+
+  ASSERT_EQ(0, partition.MakeSingleton());
+  ASSERT_THAT(representatives(partition), ElementsAre(0));
+  ASSERT_THAT(parents(partition), ElementsAre(0));
+  ASSERT_THAT(sizes(partition), ElementsAre(1));
+
+  ASSERT_EQ(1, partition.MakeSingleton());
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 1));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 1));
+  ASSERT_THAT(sizes(partition), ElementsAre(1, 1));
+
+  ASSERT_EQ(2, partition.MakeSingleton());
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 1, 2));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 1, 2));
+  ASSERT_THAT(sizes(partition), ElementsAre(1, 1, 1));
+}
+
+TEST(DisjointSets, NoOptimizations) {
+  DisjointSets partition(6, false, false);
+
+  partition.MergeSets(3, 1);
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 1, 2, 1, 4, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 1, 2, 1, 4, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(1, 2, 1, 2, 1, 1));
+
+  partition.MergeSets(1, 0);
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 0, 2, 0, 4, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 0, 2, 1, 4, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(3, 3, 1, 3, 1, 1));
+
+  partition.MergeSets(4, 2);
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 0, 2, 0, 2, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 0, 2, 1, 2, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(3, 3, 2, 3, 2, 1));
+
+  partition.MergeSets(2, 0);
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 0, 0, 0, 0, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 0, 0, 1, 2, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(5, 5, 5, 5, 5, 1));
+}
+
+TEST(DisjointSets, JustMergeByRankOptimization) {
+  DisjointSets partition(6, true, false);
+
+  partition.MergeSets(3, 1);
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 1, 2, 1, 4, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(0, 1, 2, 1, 4, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(1, 2, 1, 2, 1, 1));
+
+  partition.MergeSets(1, 0);
+  ASSERT_THAT(representatives(partition), ElementsAre(1, 1, 2, 1, 4, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(1, 1, 2, 1, 4, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(3, 3, 1, 3, 1, 1));
+
+  partition.MergeSets(4, 2);
+  ASSERT_THAT(representatives(partition), ElementsAre(1, 1, 2, 1, 2, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(1, 1, 2, 1, 2, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(3, 3, 2, 3, 2, 1));
+
+  partition.MergeSets(2, 0);
+  ASSERT_THAT(representatives(partition), ElementsAre(1, 1, 1, 1, 1, 5));
+  ASSERT_THAT(parents(partition), ElementsAre(1, 1, 1, 1, 2, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(5, 5, 5, 5, 5, 1));
+}
+
+TEST(DisjointSets, JustPathCompressionOptimization) {
+  DisjointSets partition(6, false, true);
+
+  partition.MergeSets(3, 1);
+  ASSERT_THAT(parents(partition), ElementsAre(0, 1, 2, 1, 4, 5));
+
+  partition.MergeSets(1, 0);
+  ASSERT_THAT(parents(partition), ElementsAre(0, 0, 2, 1, 4, 5));
+
+  partition.MergeSets(4, 2);
+  ASSERT_THAT(parents(partition), ElementsAre(0, 0, 2, 1, 2, 5));
+
+  partition.MergeSets(2, 0);
+  ASSERT_THAT(parents(partition), ElementsAre(0, 0, 0, 1, 2, 5));
+
+  ASSERT_THAT(representatives(partition), ElementsAre(0, 0, 0, 0, 0, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(5, 5, 5, 5, 5, 1));
+}
+
+TEST(DisjointSets, BothOptimizations) {
+  DisjointSets partition(6);
+
+  partition.MergeSets(3, 1);
+  ASSERT_THAT(parents(partition), ElementsAre(0, 1, 2, 1, 4, 5));
+
+  partition.MergeSets(1, 0);
+  ASSERT_THAT(parents(partition), ElementsAre(1, 1, 2, 1, 4, 5));
+
+  partition.MergeSets(4, 2);
+  ASSERT_THAT(parents(partition), ElementsAre(1, 1, 2, 1, 2, 5));
+
+  partition.MergeSets(0, 2);
+  ASSERT_THAT(parents(partition), ElementsAre(1, 1, 1, 1, 2, 5));
+
+  ASSERT_THAT(representatives(partition), ElementsAre(1, 1, 1, 1, 1, 5));
+  ASSERT_THAT(sizes(partition), ElementsAre(5, 5, 5, 5, 5, 1));
+}
+
+TEST(DisjointSets, HooksAreCalledAsExpected) {
+  class Partition : public DisjointSetsCrtp<Partition> {
+   public:
+    Partition(int n = 0, bool enable_merge_by_size = true,
+              bool enable_path_compression = true)
+        : DisjointSetsCrtp(n, enable_merge_by_size, enable_path_compression) {}
+
+    MOCK_METHOD(void, OnMakeSingleton, ());
+    MOCK_METHOD(void, OnMergeSets, (int, int));
+    MOCK_METHOD(void, OnCompressPath, (int, int));
+  };
+
+  {
+    InSequence s;
+
+    Partition partition(3);
+
+    EXPECT_CALL(partition, OnMakeSingleton()).Times(1);
+    ASSERT_EQ(3, partition.MakeSingleton());
+
+    EXPECT_CALL(partition, OnMergeSets(0, 1)).Times(1);
+    ASSERT_EQ(1, partition.MergeSets(0, 1));
+
+    EXPECT_CALL(partition, OnMergeSets(2, 3)).Times(1);
+    ASSERT_EQ(3, partition.MergeSets(2, 3));
+
+    EXPECT_CALL(partition, OnMergeSets(0, 2)).Times(1);
+    ASSERT_EQ(3, partition.MergeSets(0, 2));
+
+    EXPECT_CALL(partition, OnCompressPath(0, 3));
+    ASSERT_EQ(3, partition.Find(0));
+  }
+}
+
+}  // namespace cpl
