6a921643cdc928868f7b0031fb8078a35fd227b9
Łukasz Bieniasz-Krzywiec <lukaszbk@gmail.com>
2021-08-15 20:03:58 -0700

    Simplify solution for UVA 1265 - Tour Belt.

--- problems/uva/1265/solution.cc
+++ problems/uva/1265/solution.cc
@@ -14,12 +14,12 @@
 //      (1) A ⊆ B
 //      (2) B ⊆ A
 //      (3) A ∩ B = ∅
-// 2. For any two maximum spanning tree components U and V that get merged while
-//    Kruskal's algorithm is running, and for any tour belt candidate A, exactly
-//    one of the following statements is true:
+// 2. For any two maximum spanning tree components U and V that get merged by
+//    Kruskal's algorithm, and for any tour belt candidate A, exactly one of
+//    the following statements is true:
 //      (1) A ⊆ U
 //      (2) A ⊆ V
-//      (2) U ∪ V ⊆ A
+//      (3) U ∪ V ⊆ A
 
 namespace sol {
 
@@ -36,7 +36,7 @@ Graph ReadGraph(std::istream& in) {
   while (m--) {
     int u, v, k;
     in >> u >> v >> k;
-    graph.AddEdge(u, v, {.synergy = k});
+    graph.AddEdge(u - 1, v - 1, {.synergy = k});
   }
   return graph;
 }
@@ -45,11 +45,12 @@ class KruskalHooks : public cpl::KruskalHooksCrtp<Graph, KruskalHooks> {
  public:
   void OnSetUp() {
     const int n = graph_->vertex_count();
-    border_synergy_.resize(n, std::vector<int>(n, -1));
-    inside_synergy_.resize(n, std::vector<int>(n, -1));
+    min_synergy_.resize(n, std::vector<int>(n, kMaxSynergy_));
+    max_synergy_.resize(n, std::vector<int>(n, kMinSynergy_));
     for (const auto& edge : graph_->edges()) {
-      UpdateBorderSynergy(edge.source, edge.target, edge.synergy);
-      UpdateBorderSynergy(edge.target, edge.source, edge.synergy);
+      const int u = edge.source, v = edge.target;
+      min_synergy_[u][v] = min_synergy_[v][u] = edge.synergy;
+      max_synergy_[u][v] = max_synergy_[v][u] = edge.synergy;
     }
   }
 
@@ -66,21 +67,18 @@ class KruskalHooks : public cpl::KruskalHooksCrtp<Graph, KruskalHooks> {
     if (components_->size(u) > components_->size(v)) {
       std::swap(u, v);
     }
-    // Vertex v will be the representative of the newly merged component.
-    UpdateInsideSynergy(v, u, edge.synergy);
+    int max_border_synergy = kMinSynergy_;
+    int min_inside_synergy = kMaxSynergy_;
     for (int i = 0; i < graph_->vertex_count(); ++i) {
-      if (i == v) continue;
-      if (BorderEdgeExists(u, i)) {
-        if (components_->Find(i) == v) {
-          UpdateInsideSynergy(v, i, border_synergy_[u][i]);
-        } else {
-          UpdateBorderSynergy(v, i, border_synergy_[u][i]);
-        }
-      } else if (InsideEdgeExists(u, i)) {
-        UpdateInsideSynergy(v, i, inside_synergy_[u][i]);
+      min_synergy_[v][i] = std::min(min_synergy_[v][i], min_synergy_[u][i]);
+      max_synergy_[v][i] = std::max(max_synergy_[v][i], max_synergy_[u][i]);
+      if (components_->Find(i) == u || components_->Find(i) == v) {
+        min_inside_synergy = std::min(min_inside_synergy, min_synergy_[v][i]);
+      } else {
+        max_border_synergy = std::max(max_border_synergy, max_synergy_[v][i]);
       }
     }
-    if (MinBorderSynergy(v) < MaxInsideSynergy(v)) {
+    if (max_border_synergy < min_inside_synergy) {
       answer += components_->size(u) + components_->size(v);
     }
   }
@@ -88,46 +86,11 @@ class KruskalHooks : public cpl::KruskalHooksCrtp<Graph, KruskalHooks> {
   int answer = 0;
 
  private:
-  bool BorderEdgeExists(int u, int v) const {
-    return border_synergy_[u][v] >= 0;
-  }
+  const int kMinSynergy_ = 0;
+  const int kMaxSynergy_ = 1e5 + 1;
 
-  void UpdateBorderSynergy(int u, int v, int synergy) {
-    if (!BorderEdgeExists(u, v) || border_synergy_[u][v] > synergy) {
-      border_synergy_[u][v] = synergy;
-    }
-  }
-
-  bool InsideEdgeExists(int u, int v) const {
-    return inside_synergy_[u][v] >= 0;
-  }
-
-  void UpdateInsideSynergy(int u, int v, int synergy) {
-    if (!InsideEdgeExists(u, v) || inside_synergy_[u][v] < synergy) {
-      inside_synergy_[u][v] = synergy;
-    }
-    border_synergy_[u][v] = -1;
-  }
-
-  int MinBorderSynergy(int u) const {
-    int result = -1;
-    for (int i = 0; i < graph_->vertex_count(); ++i) {
-      result = std::max(result, border_synergy_[u][i]);
-    }
-    return result;
-  }
-
-  int MaxInsideSynergy(int u) const {
-    int result = 1e5;
-    for (int i = 0; i < graph_->vertex_count(); ++i) {
-      if (!InsideEdgeExists(u, i)) continue;
-      result = std::min(result, inside_synergy_[u][i]);
-    }
-    return result;
-  }
-
-  std::vector<std::vector<int>> border_synergy_;
-  std::vector<std::vector<int>> inside_synergy_;
+  std::vector<std::vector<int>> min_synergy_;
+  std::vector<std::vector<int>> max_synergy_;
 };
 
 void ProcessTestCase(std::istream& in, std::ostream& out) {
